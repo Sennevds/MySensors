e4cb0b1226fa0440fddbe154c958f9ca4d9f3936
Sennevds <senne.vandesompele@gmail.com>
2017-01-06 13:05:12 +0100

    added excel with device mapping

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,3 @@
-/**
- * The MySensors Arduino library handles the wireless radio link and protocol
- * between your home built sensors/actuators and HA controller of choice.
- * The sensors forms a self healing radio network with optional repeaters. Each
- * repeater and gateway builds a routing tables in EEPROM which keeps track of the
- * network topology allowing messages to be routed to nodes.
- *
- * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2015 Sensnology AB
- * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
- *
- * Documentation: http://www.mysensors.org
- * Support Forum: http://forum.mysensors.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- *******************************
- *
- * REVISION HISTORY
- * Version 1.0 - Henrik EKblad
- * Contribution by tekka,
- * Contribution by a-lurker and Anticimex,
- * Contribution by Norbert Truchsess <norbert.truchsess@t-online.de>
- * Contribution by Ivo Pullens (ESP8266 support)
- *
- * DESCRIPTION
- * The EthernetGateway sends data received from sensors to the WiFi link.
- * The gateway also accepts input on ethernet interface, which is then sent out to the radio network.
- *
- * VERA CONFIGURATION:
- * Enter "ip-number:port" in the ip-field of the Arduino GW device. This will temporarily override any serial configuration for the Vera plugin.
- * E.g. If you want to use the defualt values in this sketch enter: 192.168.178.66:5003
- *
- * LED purposes:
- * - To use the feature, uncomment WITH_LEDS_BLINKING in MyConfig.h
- * - RX (green) - blink fast on radio message recieved. In inclusion mode will blink fast only on presentation recieved
- * - TX (yellow) - blink fast on radio message transmitted. In inclusion mode will blink slowly
- * - ERR (red) - fast blink on error during transmission error or recieve crc error
- *
- * See http://www.mysensors.org/build/esp8266_gateway for wiring instructions.
- * nRF24L01+  ESP8266
- * VCC        VCC
- * CE         GPIO4
- * CSN/CS     GPIO15
- * SCK        GPIO14
- * MISO       GPIO12
- * MOSI       GPIO13
- * GND        GND
- *
- * Not all ESP8266 modules have all pins available on their external interface.
- * This code has been tested on an ESP-12 module.
- * The ESP8266 requires a certain pin configuration to download code, and another one to run code:
- * - Connect REST (reset) via 10K pullup resistor to VCC, and via switch to GND ('reset switch')
- * - Connect GPIO15 via 10K pulldown resistor to GND
- * - Connect CH_PD via 10K resistor to VCC
- * - Connect GPIO2 via 10K resistor to VCC
- * - Connect GPIO0 via 10K resistor to VCC, and via switch to GND ('bootload switch')
- *
-  * Inclusion mode button:
- * - Connect GPIO5 via switch to GND ('inclusion switch')
- *
- * Hardware SHA204 signing is currently not supported!
- *
- * Make sure to fill in your ssid and WiFi password below for ssid & pass.
- */
 #include <ESP8266mDNS.h>
 #include <ArduinoOTA.h>
 
@@ -82,22 +15,22 @@
 #define MY_GATEWAY_ESP8266
 
 // Set this node's subscribe and publish topic prefix
-#define MY_MQTT_PUBLISH_TOPIC_PREFIX "mygateway1-out"
-#define MY_MQTT_SUBSCRIBE_TOPIC_PREFIX "mygateway1-in"
+#define MY_MQTT_PUBLISH_TOPIC_PREFIX "mygateway3-out"
+#define MY_MQTT_SUBSCRIBE_TOPIC_PREFIX "mygateway3-in"
 
 // Set MQTT client id
-#define MY_MQTT_CLIENT_ID "mysensors-1"
+#define MY_MQTT_CLIENT_ID "mysensors-3"
 
 // Enable these if your MQTT broker requires usenrame/password
-#define MY_MQTT_USER "username"
-#define MY_MQTT_PASSWORD "password"
+#define MY_MQTT_USER "xxxx"
+#define MY_MQTT_PASSWORD "xxxxx"
 
-#define MY_ESP8266_SSID "*******"
-#define MY_ESP8266_PASSWORD "******"
+#define MY_ESP8266_SSID "xxxxxx"
+#define MY_ESP8266_PASSWORD "xxxxxx"
 
 // Set the hostname for the WiFi Client. This is the hostname
 // it will pass to the DHCP server if not static.
-// #define MY_ESP8266_HOSTNAME "sensor-ota-gateway"
+#define MY_ESP8266_HOSTNAME "Gateyway_3"
 
 // Enable UDP communication
 //#define MY_USE_UDP
@@ -106,7 +39,7 @@
 //#define MY_IP_ADDRESS 192,168,178,87
 
 // If using static ip you need to define Gateway and Subnet address as well
-#define MY_IP_ADDRESS 192,168,0,7
+#define MY_IP_ADDRESS 192,168,0,9
 #define MY_IP_GATEWAY_ADDRESS 192,168,0,1
 #define MY_IP_SUBNET_ADDRESS 255,255,255,0
 
@@ -121,15 +54,10 @@
 
 #include <SPI.h>
 #include <MySensors.h>
-#include <Bounce2.h>
 
-#define CHILD_ID 1
-#define BUTTON_PIN 3
+#define CHILD_ID 3
 
-Bounce debouncer = Bounce();
-int	oldValue=-1;
-
-MyMessage msg(CHILD_ID,V_LIGHT);
+//MyMessage msg(CHILD_ID,V_LIGHT);
 
 void setup()
 {
@@ -162,17 +90,12 @@ void setup()
 	Serial.print("IP address: ");
 	Serial.println(WiFi.localIP());
 
-	pinMode(BUTTON_PIN, INPUT);
-	digitalWrite(BUTTON_PIN,HIGH);
-	debouncer.attach(BUTTON_PIN);
-	debouncer.interval(5);
-
 }
 
 void presentation()
 {
 	// Present locally attached sensors here
-	present(CHILD_ID, S_LIGHT);
+	//present(CHILD_ID, S_LIGHT);
 }
 
 
@@ -180,12 +103,4 @@ void loop()
 {
 	// Send locally attech sensors data here
 	ArduinoOTA.handle();
-	debouncer.update();
-	int value = debouncer.read();
-
-	if(value != oldValue)
-	{
-		send(msg.set(value==HIGH ? 1 : 0));
-		oldValue = value;
-	}
 }
